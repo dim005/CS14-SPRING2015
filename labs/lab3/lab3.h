d14947a1d38bdddb82595d4022b1cba55cc0cf40
dim005 <dim005@ucr.edu>
2015-04-21 19:40:18 +0000

    a

---
+++ labs/lab3/lab3.h
@@ -0,0 +1,55 @@
+// Name: Danny
+// SID: dim005
+// Date: Apr 21, 2015
+// Approach: 
+
+#include <iostream>
+
+using namespace std;
+
+#ifndef __lab3_h__
+#define __lab3_h__
+
+class Stack
+{
+    private:
+        
+    public:
+        
+}
+
+class TwoStackFixed
+{
+    private:
+        
+    
+    public:
+        TwoStackFixed();
+        TwoStackFixed(int size, int maxtop);
+        ~TwoStackFixed();
+        
+        void pushStack1(T value);
+        void pushStack2(T value);
+        T popStack1();
+        T popStack2();
+        bool isFullStack1();
+        bool isFullStack2();
+        bool isEmptyStack1();
+        bool isEmptyStack2();
+        void display();
+}
+
+class TwoStackOptimal
+{
+    private:
+        
+    public:
+        TwoStackOptimal();
+        TwoStackOptimal(int size);
+        ~TwoStackOptimal;
+        
+        void pushFlexStack1(T value);
+        void pushFlexStack2(T value);
+}
+
+#endif
---
+++ labs/lab3/main.cc
@@ -0,0 +1,13 @@
+// Name: Danny
+// SID: dim005
+// Date: Apr 21, 2015
+// Approach: 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
